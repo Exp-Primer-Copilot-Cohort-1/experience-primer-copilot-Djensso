69762e8f82245ff3efdabd7f51c502248e03db5d
Djensso <104597826+Djensso@users.noreply.github.com>
2023-12-10 22:43:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,40 @@
-// create web server
-var express = require("express");
-var router = express.Router();
-var bodyParser = require("body-parser");
-var db = require("../models");
-var helpers = require("../helpers/comments");
+// create web server using express
+const express = require('express');
+const app = express();
+// import the comments.json file
+const comments = require('./comments.json');
+const cors = require('cors');
+// use cors
+app.use(cors());
 
-router.route("/")
-    .get(helpers.getComments)
-    .post(helpers.createComment);
+// create a GET route at /comments
+app.get('/comments', (req, res) => {
+  // send back the comments json file
+  res.send(comments);
+});
 
-router.route("/:commentId")
-    .get(helpers.getComment)
-    .put(helpers.updateComment)
-    .delete(helpers.deleteComment);
+// set up the port
+const port = process.env.PORT || 4001;
+// listen on the port
+app.listen(port, () => {
+  console.log(`Web server is listening on port ${port}!`);
+});
 
-module.exports = router;
+// import the file system module
+const fs = require('fs');
+
+// read the comments.json file
+fs.readFile('./comments.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log(`Error reading file from disk: ${err}`);
+  } else {
+    // parse JSON string to JSON object
+    const comments = JSON.parse(data);
+
+    // create a GET route at /comments
+    app.get('/comments', (req, res) => {
+      // send back the comments json file
+      res.send(comments);
+    });
+  }
+});
