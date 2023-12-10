22e5fdcbc1d477843c489f3cad62a8226bb4acbf
Djensso <104597826+Djensso@users.noreply.github.com>
2023-12-10 22:35:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,53 +1,17 @@
 // create web server
-// 1. load module
-var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+var db = require("../models");
+var helpers = require("../helpers/comments");
 
-// 2. create app
-var app = express();
-var COMMENTS_FILE = __dirname + '/comments.json';
+router.route("/")
+    .get(helpers.getComments)
+    .post(helpers.createComment);
 
-// 3. set port
-app.set('port', process.env.PORT || 3000);
+router.route("/:commentId")
+    .get(helpers.getComment)
+    .put(helpers.updateComment)
+    .delete(helpers.deleteComment);
 
-// 4. use middleware
-app.use('/', express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-// 5. create router
-app.get('/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-    res.json(JSON.parse(data));
-  });
-});
-
-app.post('/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-    var comments = JSON.parse(data);
-    var newComment = {
-      id: Date.now(),
-      // assuming you have author and text in the request body
-      author: req.body.author,
-      text: req.body.text
-    };
-
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.log(err);
-        process.exit(1);
-      }
-      res.json(comments);
-    });
-  });
-});
+module.exports = router;
